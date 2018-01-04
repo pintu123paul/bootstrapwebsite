3933dd5ff09341cfd98a42a7d48f543ab5d38f08
Pintu <pintu123paul@gmail.com>
2018-01-04 14:43:33 -0500

    first init

---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app = express();
+
+//set port
+var port = process.env.PORT || 8080
+
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+
+app.get('/', function(request, response){
+    response.render('home');
+});
+
+app.get('*', function(request, response) {
+  response.status(404).send('Page not found.');
+});
+
+app.listen(port, function () {
+    console.log('Example app listening on port 3000!');
+});
